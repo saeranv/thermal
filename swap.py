0e5fbf09c16c809a0b37b9b1e5789c035469e384
Saeran <saeranv@gmail.com>
2023-03-15 18:01:11 -0400

    Add EMS lights

--- swap.py
+++ swap.py
@@ -3,10 +3,9 @@ from collections import OrderedDict
 import os
 import sys
 pp = lambda *x: print(x, sep="\n")
-import traceback
 
 INIT_ERR_MSG = lambda m: "Not initialized error, for {}.".format(m)
-IS_TTY = len(sys.argv[0]) != 0
+IS_TTY = len(sys.argv[0]) > 0
 _LINEBREAK = "------------------------------------------"
 LINEBREAK = (_LINEBREAK, _LINEBREAK)
 
@@ -14,8 +13,6 @@ LINEBREAK = (_LINEBREAK, _LINEBREAK)
 if not IS_TTY:
     from ladybug_rhino.openstudio import load_osm, dump_osm
 else:
-    #argv = sys.argv[1:]
-    #assert len(argv) == 2, "Expected 2 args, got {}.".format(len(argv))
     # Define load_osm, dump_osm
     import openstudio as ops
     def load_osm(osm_fpath):
@@ -27,27 +24,32 @@ else:
         osm_model.save(ops.toPath(osm_fpath), True)
         return osm_fpath
 
-    run = True
-    args = sys.argv[1:]
+    run, args = True, sys.argv[1:]
     if len(args) == 0:
-        _swaps = (0, 0, 1)
+        _swaps = (0, 0, 0, 1)
     elif args[0] == "-h":
-        print("Usage: python swap.py [swap_constr] [swap_sizing] [swap_equip]")
+        print("Usage: python swap.py [swap_constr] [swap_sizing] [swap_equip]"
+              " [swap_light]")
         exit()
     else:
         _swaps = args
-    swap_constr_, swap_sizing_, swap_equip_ = [bool(int(i)) for i in _swaps]
+    swap_constr_, swap_sizing_, swap_equip_, swap_light_ = \
+        [bool(int(i)) for i in _swaps]
     _osm_fpath = os.path.join(os.getcwd(), "act/act.osm")
     _ref_osm_fpath = os.path.join(os.getcwd(), "ref/ref.osm")
     assert os.path.exists(_osm_fpath), os.path.abspath(_osm_fpath)
     assert os.path.exists(_ref_osm_fpath), os.path.abspath(_ref_osm_fpath)
 
 
-def ppdir(modelobj, qstr=""):#, *args, **kwargs):
+def ppdir(modelobj, qstr="", *args, **kwargs):
     """Helper function to pretty print directories."""
+
     def _cond_fn(x):
         x = x.lower()
-        return (not x.startswith("__")) and (qstr.lower() in x)
+        return (
+            not x.startswith("__") and \
+            qstr.lower() in x
+        )
 
     def modelobj_method_strs(obj, cond_fn, result):
         result += [mstr for mstr in dir(obj) if cond_fn(mstr)]
@@ -58,7 +60,7 @@ def ppdir(modelobj, qstr=""):#, *args, **kwargs):
             return modelobj_method_strs(parent, cond_fn, result)
 
     result = modelobj_method_strs(modelobj, _cond_fn, [])
-    print(*result)#, *args, **kwargs)
+    print(*result, *args, **kwargs)
 
 
 def argmin(arr):
@@ -94,6 +96,15 @@ def match_srf(act_srf, ref_areas, area_eps):
     ri = argmin(diffs)
     ri_diff = _diff_fn(ref_areas[ri], act_srf)
     return _assert_eps(ri, ri_diff, area_eps)
+
+
+def match_spc(rspcs, aspcs):
+    rspcs = sorted(rspcs, key=lambda x: x.nameString())
+    aspcs = sorted(aspcs, key=lambda x: x.nameString())
+    for rspc, aspc in zip(rspcs, aspcs):
+        assert rspc.nameString() in aspc.nameString(), \
+            "Names don't match in spc."
+    return rspcs, aspcs
 
 
 def modelobj_set_fn(property_fn, property_set):
@@ -249,6 +260,7 @@ def attr_dict(modelobj, attrs):
     """Get ordered dictionary given attributes from modelobj."""
     return OrderedDict([(attr, getattr(modelobj, attr)()) for attr in attrs])
 
+
 # TODO: cross-entropy of attri
 def hist(values):
     """Histogram of attribute values."""
@@ -311,67 +323,140 @@ def swap_spc_equip(act_osm, ref_osm, verbose=False):
     #    print("Equipments are equal. No swapping.")
     #    return act_osm
     print("Equipments not equal. Swapping.")
-    def _match_spc(rspcs, aspcs):
-        rspcs = sorted(rspcs, key=lambda x: x.nameString())
-        aspcs = sorted(aspcs, key=lambda x: x.nameString())
-        for rspc, aspc in zip(rspcs, aspcs):
-            assert rspc.nameString() in aspc.nameString(), \
-                "Names don't match in spc."
-        return rspcs, aspcs
 
     # Get all spaces, and sort them
     ref_spcs, act_spcs = ref_osm.getSpaces(), act_osm.getSpaces()
-    ref_spcs, act_spcs = _match_spc(ref_spcs, act_spcs)
+    ref_spcs, act_spcs = match_spc(ref_spcs, act_spcs)
 
     ## Loop through spaces and remove/then assign eqiuip_defn to space
     for ref_spc, act_spc in zip(ref_spcs, act_spcs):
-        for act_equip in act_spc.electricEquipment():
-            pass #act_equip.remove()
+        # TODO: check if parent spacetype or space and check accordingly
+        #for act_equip in act_spc.electricEquipment():
+        #    parent = assert_init(act_equip.parent()).get()
+        #    print(type(parent.iddObjectType()))
         for ref_equip in ref_spc.electricEquipment():
             ## TODO: add the diff-check which adds unique_ref_equip
-            ## unique_ref_equip = ref_equip not in (act_equip AND ref_equip)
-            #if "elevator" not in ref_equip.nameString().lower():
-            #    continue
+            ## i.e. unique_ref_equip = ref_equip not in (act_equip AND ref_equip)
+            if "elevator" not in ref_equip.nameString().lower():
+                continue
             act_equip = swap_modelobj(ref_equip, act_osm)
             is_parent_set = act_equip.setParent(act_spc)
             assert is_parent_set, \
                 "Error! setParent fail for {}".format(act_equip)
             print('Added equip: {} to space: {}'.format(
                 act_equip.nameString(), act_spc.nameString()))
-    #swap_spctype_equip(act_osm, ref_osm)
 
     return act_osm
 
-def swap_spctype_equip(act_osm, ref_osm):
-    """Swap spctype equip."""
 
-    ref_equips = ref_osm.getElectricEquipments()
-    act_equips = act_osm.getElectricEquipments()
-    act_names = [act.nameString() for act in act_equips]
-    act_equip = act_equips[0]
-    parent = assert_init(act_equip.parent()).get()
-    print(type(parent.iddObjectType()))
-    #ppdir(parent, sep='\n')
-    #for ref_equip in ref_equips:
-    #
-    #    ref_parent = assert_init(ref_equip.parent()).get()
-    #ref_spc_types = ref_osm.getSpaceTypes()
-    #print(ref_light.lightsDefinition())
+def swap_hvac(act_osm, ref_osm):
+    """Swap hvac."""
+
+    def _diff_hvac(act_osm, ref_osm):
+        """Compare hvac."""
+        pass
+
+    # TODO: check if hvac objects exist using lib from idf
 
 
-def swap_light_spc(act_osm, ref_osm):
-    """Swap lights from spc."""
-    # TODO: define to space not spacetype:
-    ref_lights = ref_osm.getLightss()
-    for ref_light in ref_lights:
-        ref_parent = assert_init(ref_light.parent()).get()
-    ref_spc_types = ref_osm.getSpaceTypes()
-    print(ref_light.lightsDefinition())
-    #print(*ref_spc_types, sep='\n')
+def swap_spc_light(act_osm, ref_osm):
+    """Swap space lighting."""
+
+    ## Get resources
+    light_sched_name = "OfficeMedium BLDG_LIGHT_SCH_2013"
+    #ref_sched_opt = ref_osm.getScheduleByName(light_sched_name)
+    #ref_sched = assert_init(ref_sched_opt).get()
+    #act_sched = swap_modelobj(ref_sched, act_osm)
+
+    # Get all spaces, and sort them
+    ref_spcs, act_spcs = ref_osm.getSpaces(), act_osm.getSpaces()
+    ref_spcs, act_spcs = match_spc(ref_spcs, act_spcs)
+
+    ## Loop through spaces and remove/then assign light_defn to space
+    for ref_spc, act_spc in zip(ref_spcs, act_spcs):
+        ref_name, act_name = ref_spc.nameString(), act_spc.displayName()
+        ref_spc_type = assert_init(ref_spc.spaceType()).get()
+        act_spc_type = assert_init(act_spc.spaceType()).get()
+
+        # DaylightSensors
+        ref_controls = ref_spc.daylightingControls()
+        if len(ref_controls) == len(act_spc.daylightingControls()):
+            continue
+
+        # Space lights
+        for act_spc_light in act_spc.lights():
+            act_spc_light.remove()
+        for ref_type_light in ref_spc_type.lights():
+            # Get sched
+            ref_light = assert_init(ref_type_light.to_Lights()).get()
+            act_light_opt = swap_modelobj(ref_light, act_osm)
+            act_light = assert_init(act_light_opt.to_Lights()).get()
+            is_parent_set = act_light.setParent(act_spc)
+            assert is_parent_set, "setParent fail for {}".format(act_light)
+            # Swap schedule
+            act_sched_opt = act_osm.getScheduleByName(light_sched_name)
+            act_sched = assert_init(act_sched_opt).get()
+            is_sched = act_light.setSchedule(act_sched)
+            print('Added {} to space: {}'.format(act_light.nameString(), act_name))
+
+        for ref_control in ref_controls:
+            act_control = swap_modelobj(ref_control, act_osm)
+            is_parent_set = act_control.setParent(act_spc)
+            assert is_parent_set, "setParent fail for {}".format(act_control)
+            print('Added {} to space: {}'.format(
+                  act_control.nameString(), act_name))
+    ems_data = {
+        'sensor_name': [],
+        'actuated_comp': []
+    }
+
+    for ems_sensor in ref_osm.getEnergyManagementSystemSensors():
+        ems_sensor_var = ems_sensor.outputVariableOrMeterName()
+        if "Lights" not in ems_sensor_var:
+            continue
+        act_ems_sensor = swap_modelobj(ems_sensor, act_osm)
+        ems_data['sensor_name'] += [act_ems_sensor.nameString()]
+    for ems_actuator in ref_osm.getEnergyManagementSystemActuators():
+        if "Lights" not in ems_actuator.actuatedComponentType():
+            continue
+        act_ems_actuator = swap_modelobj(ems_actuator, act_osm)
+        act_ems_actuator = act_ems_actuator.to_EnergyManagementSystemActuator()
+        act_ems_actuator = assert_init(act_ems_actuator).get()
+        #comp = assert_init(act_ems_actuator.actuatedComponent()).get()
+        #ems_data['actuated_comp'] += [comp.nameString()]
+    for ems_glob_var in ref_osm.getEnergyManagementSystemGlobalVariables():
+        swap_modelobj(ems_glob_var, act_osm)
+    #for ems_out_var in ref_osm.getEnergyManagementSystemOutputVariables():
+    #    swap_modelobj(ems_out_var, act_osm)
+    for ems_int_var in ref_osm.getEnergyManagementSystemInternalVariables():
+        swap_modelobj(ems_int_var, act_osm)
+    # Programs
+    for ems_prog_call in ref_osm.getEnergyManagementSystemProgramCallingManagers():
+        ems_prog_call_name = ems_prog_call.nameString()
+        if "Light" not in ems_prog_call_name:
+            continue
+        act_prog_call = swap_modelobj(ems_prog_call, act_osm)
+        act_prog_call = act_prog_call.to_EnergyManagementSystemProgramCallingManager()
+        act_prog_call = assert_init(act_prog_call).get()
+        # Swap program
+        ems_prog = assert_init(ems_prog_call.getProgram(0)).get()
+        act_prog = swap_modelobj(ems_prog, act_osm)
+        act_prog = assert_init(act_prog.to_EnergyManagementSystemProgram()).get()
+        act_prog_call.eraseProgram(0)
+        act_prog_call.addProgram(act_prog)
+    #for ems_prog in ref_osm.getEnergyManagementSystemPrograms():
+    #    swap_modelobj(ems_prog, act_osm)
+    #ems_progs = act_osm.getEnergyManagementSystemPrograms()
+    #ems_prog_calls = act_osm.getEnergyManagementSystemProgramCallingManagers()
+    #for ems_prog, ems_prog_call in zip(ems_progs, ems_prog_calls):
+    #    ems_prog_call.eraseProgram(0)
+    #    ems_prog_call.addProgram(ems_prog)
+
+    return act_osm
 
 
 def main(act_fpath, ref_fpath, act_swap_fpath,
-         is_swap_constr, is_swap_sizing, is_swap_equip):
+         is_swap_constr, is_swap_sizing, is_swap_equip, is_swap_light):
     """Main function."""
 
     swap_srf_types = {"Floor"}
@@ -398,13 +483,17 @@ def main(act_fpath, ref_fpath, act_swap_fpath,
     if is_swap_equip:
         print("{}\nSWAP EQUIP\n{}".format(*LINEBREAK))
         act_model = swap_spc_equip(act_model, ref_model, verbose=True)
+    # SWAP LIGHT
+    if is_swap_light:
+        print("{}\nSWAP INTERIOR-LIGHT\n{}".format(*LINEBREAK))
+        act_model = swap_spc_light(act_model, ref_model)
 
     ## Dump osm model
     act_swap_fpath = dump_osm(act_model, act_swap_fpath)
     return act_swap_fpath
 
-if run:
 
+if run:
     if IS_TTY:
         act_swap_fpath_ = _osm_fpath.replace('.osm', '_swap.osm')
     else:
@@ -419,13 +508,15 @@ if run:
     swap_constr_ = False if swap_constr_ is None else swap_constr_
     swap_sizing_ = False if swap_sizing_ is None else swap_sizing_
     swap_equip_ = False if swap_equip_ is None else swap_equip_
+    swap_light_ = False if swap_light_ is None else swap_light_
     try:
         osm_fpath_edit = \
             main(_osm_fpath, _ref_osm_fpath, act_swap_fpath_,
-                 swap_constr_, swap_sizing_, swap_equip_)
+                 swap_constr_, swap_sizing_, swap_equip_, swap_light_)
         print("swap_constr:", swap_constr_,
               "swap_sizing:", swap_sizing_,
-              "swap_equip: ", swap_equip_)
+              "swap_equip: ", swap_equip_,
+              "swap_light: ", swap_light_)
         print('Edited file:', osm_fpath_edit)
 
     except Exception as err:
--- thermal.sh
+++ thermal.sh
@@ -2,16 +2,15 @@
 # /thermal-mass/thermal.sh
 
 cd "$(dirname "$0")"
-echo "thermal.sh: $(pwd)"
 THERMWIN_DP="/mnt/c/users/admin/master/thermal"
 THERM_DP=$PWD
 
 # REF VARS
 REF_DP="$THERM_DP/ref"
 REF_SEED_DP="$REF_DP/ref_seed/run"
-REF_DOE_DP="$REF_SEED_DP/000_CreateDOEPrototypeBuilding/output/SR1"
+REF_DOE_DP="$REF_SEED_DP/001_CreateDOEPrototypeBuilding/output/SR1"
 # NOTE: only use REF_DOE for osw, epw the .osm is empty!
-REF_SEED_OSW="$REF_DOE_DP/in.osw"
+REF_SEED_OSW="$REF_SEED_DP/../workflow.osw"
 REF_SEED_EPW="$REF_DOE_DP/in.epw"
 REF_SEED_OSM="$REF_SEED_DP/in.osm"
 REF_OSM="$REF_DP/ref.osm"
@@ -40,7 +39,7 @@ EOF
 )
 HB_MODEL_NAME="${HB_MODEL_DP##*/}"
 ACT_SEED_DP="$HB_MODEL_DP/openstudio/run"
-ACT_SEED_OSW="$REF_OSW" 
+ACT_SEED_OSW="$REF_SEED_OSW" 
 ACT_SEED_EPW="$REF_EPW"
 ACT_SEED_OSM="$ACT_SEED_DP/in.osm"
 ACT_OSM="$ACT_DP/act.osm"
@@ -69,9 +68,11 @@ modify_osw () {
 python - << EOF
 # Rewrite .osm seed location in given .osw
 import json; from pprint import pprint
-osw_fp = '$1'
-osm_fp = '$2'
-filter_m = lambda m: 'OpenStudio Results' not in m['name'] 
+incl_set = {'remove_orphan_objects_and_unused_resources'}
+# 'AddOutputDiagnostics', 'CreateDOEPrototypeBuilding', 
+# 'EnvelopeAndInternalLoadBreakdown'
+is_incl = lambda m: m['measure_dir_name'] in incl_set
+osw_fp, osm_fp = '$1', '$2'
 with open(osw_fp, 'r') as f:
     data = json.load(f)
 with open(osw_fp, 'w') as f:    
@@ -80,8 +81,10 @@ with open(osw_fp, 'w') as f:
     if 'weather_file' in data:
         data.pop('weather_file')
     data['weather_file'] = './in.epw'
-    data['steps'] = list(filter(filter_m, data['steps']))
     data['seed_file'] = f'../{osm_fp}.osm'
+    # Add measures 
+    data['steps'] = [mea_dict for mea_dict in data['steps'] 
+                     if is_incl(mea_dict)]
     json.dump(data, f, indent=4)
 pprint(data)
 EOF
@@ -111,7 +114,8 @@ cp_mod () {
     MOD_SEED_OSM=$5; MOD_OSM=$6
     MOD_SEED_EPW=$7; MOD_EPW=$8
     MOD_NAME=$9
-    
+    #rm_mea="remove_orphan_objects_and_unused_resources"
+
     if [ ! -d "$MOD_SEED_DP" ]; then
         echo "Error. The $MOD_SEED_DP directory does not exist."
         exit 1
@@ -123,13 +127,15 @@ cp_mod () {
 
     if [ ! -d "$MOD_DP/$MOD_NAME" ]; then
         mkdir "$MOD_DP/$MOD_NAME"
-        echo "Creating ./$MOD_NAME/$MOD_NAME directory."
+    fi
+    if [ ! -d "$MOD_DP/$MOD_NAME/measures" ]; then
+        mkdir "$MOD_DP/$MOD_NAME/measures"
     fi
 
     cp "$MOD_SEED_OSW" "$MOD_OSW"
     cp "$MOD_SEED_EPW" "$MOD_EPW"
     cp "$MOD_SEED_OSM" "$MOD_OSM"
-    
+    cp -r "$THERM_DP/measures/" "$MOD_DP/$MOD_NAME/"
     echo "Rewriting paths in ./$MOD_NAME/$MOD_NAME/workflow.osm"
     modify_osw $MOD_OSW "$MOD_NAME"
 }
@@ -143,24 +149,30 @@ swap_cp () {
     cp "$THERM_DP/ref/ref.osm" "$THERMWIN_DP/"
     cp "$THERM_DP/act/act_swap.osm" "$THERMWIN_DP/"
 }
-#mk_hb_model_swap () {
-#    # Create HB_MODEL_DIR for future swap model 
-#    HB_SWAP_DP="${HB_MODEL_DP}_Swap" 
-#    if [ ! -d "$HB_SWAP_DP" ]; then
-#        cp -r "$HB_MODEL_DP" "$HB_SWAP_DP"
-#    else
-#        cp -u "ACT_OSM" "$HB_SWAP_DP/openstudio/run/in.osm"
-#    fi
-#}
+
+ops_sim () {
+    if [[ "$#" -ne 1 ]]; then 
+        echo "Error, ops_sim requires 1 arg." && exit 1
+    fi
+    if [[ ! -f "$1" ]]; then 
+        echo "Error, $1 is not a file." && exit 1
+    fi
+    osw_fp=$1
+    openstudio run -w "$osw_fp" --no-lifecyclecosts
+    tail -3 "$(dirname $osw_fp)/run/eplusout.err"
+}
+
+# ----------------------------------
+# ARGPARSE
+# ----------------------------------
 
 helpcmd=$(python -c 'print("""thermal [-h] [-cp] [-rm] [-ls]
 Example:
     thermal ref -rm -cp
     thermal act -rm -cp
-    thermal ref|act -ls
+    thermal act -sim
 -cp: copies ref.osm|act.osm and ref/|act/ support folder.
 -rm: remove ref.osm|act.osm and ref/|act/ support folder.
--ls: lists files in ref|act dirs
 """)')
 
 
@@ -184,24 +196,11 @@ if [[ "$#" -eq 0 ]]; then
     exit 1
 fi
 
-if [[ "$1" == "-swap_sim" ]]; then
-    echo "Create ./act/act_swap dir"
-    ACT_SWAP_DP="$ACT_DP/act_swap"
-    [[ -d "$ACT_SWAP_DP" ]] && \
-        rm -rf "$ACT_SWAP_DP"
-    mkdir "$ACT_SWAP_DP"
-    cp "$ACT_OSW" "$ACT_SWAP_DP/workflow.osw"
-    cp "$ACT_EPW" "$ACT_SWAP_DP/in.epw"
-    modify_osw "$ACT_SWAP_DP/workflow.osw" "act_swap"
-    echo "Simulate ./act/act_swap"
-    openstudio run -w "$ACT_SWAP_DP/workflow.osw"
-    tail -3 "$ACT_SWAP_DP/run/eplusout.err"
-    swap_cp  
-    exit 1
-elif [[ "$1" == "-swap_cp" ]]; then
+if [[ "$1" == "-swap_cp" ]]; then
     swap_cp
     exit 1
 fi
+
 
 # for arg must be alone on this line
 for i in $@; do
@@ -213,17 +212,34 @@ for i in $@; do
             cp_mod ${ACT_VARS[@]}
     fi
     if [[ "$i" == "-sim" ]]; then
-        echo "Simulating ref/ref" 
-        openstudio run -w "$REF_OSW"
+        if [[ $STDIN_VAR == "ref" ]]; then
+            ops_sim "$REF_OSW"
+        elif [[ $STDIN_VAR == "act" ]]; then
+            ACT_SWAP_DP="$ACT_DP/act_swap"
+            [[ -d "$ACT_SWAP_DP" ]] && rm -rf "$ACT_SWAP_DP"
+            mkdir "$ACT_SWAP_DP"
+            cp "$ACT_OSW" "$ACT_SWAP_DP/workflow.osw"
+            cp "$ACT_EPW" "$ACT_SWAP_DP/in.epw"
+            modify_osw "$ACT_SWAP_DP/workflow.osw" "act_swap"
+            echo "Simulate ./act/act_swap"
+            ops_sim "$ACT_SWAP_DP/workflow.osw"
+            swap_cp
+        fi
     fi
     if [[ "$i" == "-rm" ]]; then 
         [[ $STDIN_VAR == "ref" ]] && \
             rm_mod "$REF_DP/ref" "$REF_OSM" 'ref' || \
             rm_mod "$ACT_DP/act" "$ACT_OSM" 'act'
     fi
-    if [[ "$i" == "-ls" ]]; then 
-        [[ $STDIN_VAR == "ref" ]] && \
-            ls "$THERM_DP/ref" || \
-            ls "$THERM_DP/act"
+    if [[ "$i" == "-err" ]]; then
+        if [[ $STDIN_VAR == "ref" ]]; then
+            echo "$THERM_DP/ref/ref/run/eplusout.err"
+        elif [[ $STDIN_VAR == "act" ]]; then
+            echo "$THERM_DP/act/act/run/eplusout.err"
+        elif [[ $STDIN_VAR == "swap" ]]; then
+            echo "$THERM_DP/act/act_swap/run/eplusout.err"
+        else
+            echo "Error, must specify ref|act|swap"
+        fi
     fi
 done
