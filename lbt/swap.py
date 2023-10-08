5e6086c42e534aa4d4a372c4c422714f89ac38af
Saeran <saeranv@gmail.com>
2023-10-08 07:39:22 -0400

    Cleaning up directory

--- lbt/swap.py
+++ lbt/swap.py
@@ -213,11 +213,11 @@ if __name__ == "__main__":
 
     # Version
     # Define inputs args
-    print("Swap v0.0.3")
     paths, echo = argv[1:], True
     is_help = len(paths) < 3 or paths[0] in {'-h', '--help'}
     if is_help:
-        print("Usage: python swap.py [osw] [osm] [epw]")
+        # print("Usage: python swap.py [osw] [osm] [epw]")
+        print("Swap v0.0.4")
         exit(1)
 
     # Get paths from args, make swap fpaths
